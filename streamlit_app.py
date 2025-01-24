e3555b203e78bf3a37eb7d6e449301c96292676d
thiagocaetanomedeiros <thiago.caetano.ms@gmail.com>
2025-01-24 14:49:10 -0300

    st.dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt # type: ignore
 
 filepath = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
 df = pd.read_csv(filepath, header=None)
-df.head()
+st.dataframe(df)
 
 
 st.title("Outros métodos")
