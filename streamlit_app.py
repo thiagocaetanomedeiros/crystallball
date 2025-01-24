69a5837a83d053a37b1612596d788ab14880b070
thiagocaetanomedeiros <thiago.caetano.ms@gmail.com>
2025-01-24 14:50:38 -0300

    numeric data

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt # type: ignore
 filepath = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
 df = pd.read_csv(filepath, header=None)
 st.dataframe(df)
+df=df._get_numeric_data()
+st.dataframe(df)
+
 
 
 st.title("Outros métodos")
