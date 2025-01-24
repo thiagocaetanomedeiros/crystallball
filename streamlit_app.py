b922bffad0b2466ddb7d6b9d0b1ad9dae619303e
thiagocaetanomedeiros <thiago.caetano.ms@gmail.com>
2025-01-24 14:46:31 -0300

    df.head

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,14 @@ import matplotlib.pyplot as plt # type: ignore
 
 filepath = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
 df = pd.read_csv(filepath, header=None)
+df.head()
 
 
 st.title("Outros métodos")
 
 _ = '''
 
-X. MODEL SELECTION
+X. Model Selection
 
 X.1. Model Evaluation and Refinement
 
