d6aea11e8cf3961666d8baadae2a7700f3e9a0cf
thiagocaetanomedeiros <thiago.caetano.ms@gmail.com>
2025-01-24 12:23:42 -0300

    correction on pyplot

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
-import matplotlib.plyplot as plt # type: ignore
+import matplotlib.pyplot as plt # type: ignore
 
 from pyodide.http import pyfetch # type: ignore
 
