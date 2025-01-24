1d9b5105900b4a224b364eb721a98319e3587e57
thiagocaetanomedeiros <thiago.caetano.ms@gmail.com>
2025-01-24 14:58:24 -0300

    quwmsabe

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt # type: ignore
 filepath = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
 df = pd.read_csv(filepath, header=None)
 #df.drop(['Unnamed: 0.1', 'Unnamed: 0'], axis=1, inplace=True)
-df=df._get_numeric_data()
+#df=df._get_numeric_data()
 st.dataframe(df)
 
 
