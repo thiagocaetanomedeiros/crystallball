06ff1b769bcd23b5e2213f337ccc80e2fb210b91
thiagocaetanomedeiros <thiago.caetano.ms@gmail.com>
2025-01-24 14:43:45 -0300

    pyodide down

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt # type: ignore
 
-from pyodide.http import pyfetch # type: ignore
-
 
 filepath = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
 df = pd.read_csv(filepath, header=None)
