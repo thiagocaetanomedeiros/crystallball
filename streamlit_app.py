925c189d4095592446a17e0260cf3f4429b03265
thiagocaetanomedeiros <thiago.caetano.ms@gmail.com>
2025-05-19 19:34:58 -0300

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,38 +20,63 @@ st.title("Crystall Ball - Que investimento dará maior resultado?")
 st.write(
     
     '''
-    ## Renda Fixa:
-    ### Títulos Prefixados (Bullet)
-    #### Cálculo da Taxa Efetiva Anual
+    ## 1. Renda Fixa:
+    ### 1.1. Títulos Prefixados (Bullet)
+    #### 1.1.1. Cálculo da Taxa Efetiva Anual
+    A taxa efetiva anual é a taxa de retorno que um investidor pode esperar ao manter um título prefixado até o vencimento. Ela é calculada com base no valor nominal do título, no preço unitário e no número de dias úteis até o vencimento.
     '''
  
 )
 
-
-
+# Cálculo da taxa efetiva anual
 valor_nominal   = st.number_input("Valor nominal:", min_value=0.0)
 preco_unitario  = st.number_input("Preço Unitário:", min_value=0.0)
 dias_uteis      = st.number_input("Dias úteis até o vencimento:", min_value=0)
 
-# Cálculo da taxa efetiva anual
-if st.button("Calcular Taxa Efetiva Anual"):
+if st.button("Calcular Valor Nominal"):
     taxa = fr.taxa_efetiva_anual(valor_nominal, preco_unitario, dias_uteis)
     st.success(f"A taxa efetiva anual é: {taxa:.2f}%")    
+
+
+
+# Campos de cálculo de preço unitario
+
+st.write(
+    
+    '''
+    #### 1.1.1. Cálculo do Preço Unitário
+    O preço unitário é o valor que um investidor paga por um título prefixado. Ele é calculado com base no valor nominal do título, na taxa efetiva anual e no número de dias úteis até o vencimento.
+    A fórmula para calcular o preço unitário é a seguinte:
+    $PU=\frac{VN}{[\frac{Taxa}{100}+1]^{\frac{du}{252}}}$
+    '''
+ 
+)
+
+valor_nominal   = st.number_input("Valor nominal:", min_value=0.0)
+taxa_anual      = st.number_input("Taxa Anual:", min_value=0.0)
+dias_uteis      = st.number_input("Dias úteis até o vencimento:", min_value=0)
+
+if st.button("Calcular Preço Unitário"):
+    pu = fr.preco_unitario(valor_nominal, taxa_anual, dias_uteis)
+    st.success(f"O preço unitário é : {pu:.2f}")    
+
+
+
 
 
 st.write(
     
     '''
-    ## Próximas funcionalidades:
-    I. Cálculo da taxas, retornos e preços unitários de títulos prefixados
-    II. Cálculo da taxa de retorno de títulos pós-fixados 
-    III. Webscrapping de índices financeiros
-        1) Pesquisar e criar no Python uma lista de sites para a coleta destes
-        indicadores. (Célula de Texto)
-        OBS.: com minhas sugestões ou outros de sua preferência.
-        2) Criar um código Python que interaja com o usuário coletando os valores
-        dos índices indicados neste documento. Devem ser salvos em uma lista
-            e exibidos no final para verificação.
+    ## Próximas funcionalidades:\n
+    I. Cálculo da taxas, retornos e preços unitários de títulos prefixados\n
+    II. Cálculo da taxa de retorno de títulos pós-fixados\n
+    III. Webscrapping de índices financeiros\n
+        1) Pesquisar e criar no Python uma lista de sites para a coleta destes\n
+        indicadores. (Célula de Texto)\n
+        OBS.: com minhas sugestões ou outros de sua preferência.\n
+        2) Criar um código Python que interaja com o usuário coletando os valores\n
+        dos índices indicados neste documento. Devem ser salvos em uma lista\n
+            e exibidos no final para verificação.\n
         
     '''
  
